c34898a76de385778d7a9644dd7bdbe4fa7a7751
zhengzhang <zhengzhang@ruijie.com.cn>
2023-07-02 15:12:22 +0800

    发布一级路径

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,13 @@ set -e
  
 # 生成静态文件
 npm run build
-rm -rf ../blog/*
+rm -rf ../zz-html.github.io/*
 
 # 将build生成的dist目录拷贝至上一层目录中
-cp -rf dist/* ../blog/
+cp -rf dist/* ../zz-html.github.io/
 
 # 进入生成的文件夹
-cd ../blog
+cd ../zz-html.github.io
 
 # git初始化，每次初始化不影响推送
 git init
@@ -20,4 +20,4 @@ git commit -m 'deploy'
 git branch -M main
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:zz-html/blog.git main
+git push -f git@github.com:zz-html/zz-html.github.io.git main
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ const sidebarConf = require('./config/sidebar')
 const pluginsConf = require('./config/plugins/index')
 module.exports = {
     //注意，此处需要填写你部署在nginx下的文件夹名称，如果是根目录，那么可以注释掉此行，注释掉后本地打开index.html无法访问
-    base: "/blog/",
+    //base: "/blog/",
     title: "小虎聊编程",
     description: '点赞、转发、收藏',
     dest: './dist',
